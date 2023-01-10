e0af2f3e24a850f14fb76b0cd1b629cad8204cfa
Yohan Sandun Kumaranayake <yohan.sandunk@gmail.com>
2023-01-10 21:29:04 +0530

    Update Singleton.java

--- src/creational/singleton/Singleton.java
+++ src/creational/singleton/Singleton.java
@@ -4,6 +4,10 @@ public class Singleton {
 
     private static Singleton instance;
 
+    private Singleton() {
+        
+    }
+
     public static Singleton getInstance() {
         if (instance == null)
             instance = new Singleton();
