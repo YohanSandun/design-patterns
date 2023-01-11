e6b34635fe34fe5c1d22fea01b09ed3c73ae2567
Yohan Sandun Kumaranayake <yohan.sandunk@gmail.com>
2023-01-11 22:37:54 +0530

    Create command pattern

---
+++ src/behavioral/command/CommandPatternTest.java
@@ -0,0 +1,9 @@
+package behavioral.command;
+
+public class CommandPatternTest {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
--- src/creational/singleton/Singleton.java
+++ src/creational/singleton/Singleton.java
@@ -5,7 +5,7 @@ public class Singleton {
     private static Singleton instance;
 
     private Singleton() {
-        
+
     }
 
     public static Singleton getInstance() {
